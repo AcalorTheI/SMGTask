1cca518c247a03c3b6520711730cb9163f7a0afe
Aleksandar Ivanovic <aleksandarivanovicmaster@gmail.com>
2025-06-19 12:44:01 +0200

    Initial commit

---
+++ src/main/java/com/example/SMGCodingAssesment/SmgCodingAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SMGCodingAssesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmgCodingAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmgCodingAssesmentApplication.class, args);
+	}
+
+}
